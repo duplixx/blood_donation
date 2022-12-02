cd62dc3e18b1fc9bbf29479979f517f8e3c40b77
duplixx01 <duplixxx20@gmail.com>
2022-12-02 21:58:37 +0530

    animation added

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Form() {
   return (
     <div>
-          <form className="w-full p-4">
+          <form className="w-full p-4" id="form">
               <div className="flex flex-wrap flex-col -mx-3 mb-6">
                   <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                       <label
@@ -12,44 +12,57 @@ export default function Form() {
                       >
                           Address
                       </label>
-                      <input
+                      <textarea
                           className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                           id="grid-first-name"
                           type="text"
-                          placeholder="address"
+                          placeholder="Address"
                       />
                       <p className="text-red-500 text-xs italic">Please fill out this field.</p>
                   </div>
-                  <div className="w-full md:w-1/2 px-3">
+              </div>
+
+              <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0 ">
                       <label
                           className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          htmlFor="grid-last-name"
+                          htmlFor="grid-city"
                       >
-                          Last Name
+                          Latitude
                       </label>
                       <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                          id="grid-last-name"
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="grid-city"
                           type="text"
-                          placeholder="Doe"
+                          placeholder=""
                       />
+                      <label
+                      className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="grid-city"
+                  >
+                     Longitude
+                  </label>
+                  <input
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-city"
+                      type="text"
+                      placeholder=""
+                  />
                   </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
               </div>
               <div className="flex flex-wrap -mx-3 mb-2">
                   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label
-                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 "
                           htmlFor="grid-city"
                       >
                           City
                       </label>
                       <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-city"
                           type="text"
-                          placeholder="Albuquerque"
+                          placeholder=""
                       />
                   </div>
                   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -61,22 +74,13 @@ export default function Form() {
                       </label>
                       <div className="relative">
                           <select
-                              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                              className="block appearance-none w-full bg-gray-200 border-2 border-gray-500 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                               id="grid-state"
                           >
                               <option>New Mexico</option>
                               <option>Missouri</option>
                               <option>Texas</option>
                           </select>
-                          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                              <svg
-                                  className="fill-current h-4 w-4"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 20 20"
-                              >
-                                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                              </svg>
-                          </div>
                       </div>
                   </div>
                   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -87,13 +91,14 @@ export default function Form() {
                           Zip
                       </label>
                       <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-zip"
                           type="text"
                           placeholder={90210}
                       />
                   </div>
               </div>
+              <button type='submit' className='btn btn-outline w-full flex-end mt-15'>Submit</button>
           </form>
   
     </div>
